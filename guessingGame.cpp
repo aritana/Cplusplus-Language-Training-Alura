c7487c54e771e8a0a7d47569b851e94c0ccd8e58
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-04-17 14:04:19 -0300

    starting with the language

---
+++ guessingGame.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    cout << "**************************************" << endl;
+    cout << "* Welcome to the Guessing Game! *" << endl;
+    cout << "**************************************" << endl;
+
+    int secretNumber = 42;
+
+    cout << "The secret number is " << secretNumber << ". Dont tell anyone" << endl;
+
+    return 0;
+}
