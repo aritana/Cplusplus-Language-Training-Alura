ab3ba763289e0e5618c9a4cb6704bff39fdda76f
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-04-17 19:09:20 -0300

    02:checking attempts

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,15 +1,46 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
-
     cout << "**************************************" << endl;
     cout << "* Welcome to the Guessing Game! *" << endl;
     cout << "**************************************" << endl;
 
-    int secretNumber = 42;
+    const int SECRET_NUMBER = 42;
+    bool didnotHit = true;
+    int attempts = 0;
 
-    cout << "The secret number is " << secretNumber << ". Dont tell anyone" << endl;
+    do
+    {
+        int guess;
+        attempts++;
+        cout << "Tentativa " << attempts << endl;
+
+        cout << "What is your guess? ";
+        cin >> guess;
+        cout << "The guess value is: " << guess << endl;
+
+        bool hit = (guess == SECRET_NUMBER);
+        bool greater = (guess > SECRET_NUMBER);
+
+        if (hit)
+        {
+            cout << "Congratulations! You guessed corretly!" << endl;
+            didnotHit = false;
+        }
+        else if (greater)
+        {
+            cout << "You guess is greater than the secret number!" << endl;
+        }
+        else
+        {
+            cout << "You guess is less than the secret number!" << endl;
+        }
+    } while (didnotHit);
+
+    cout << "Fim de Jogo!" << endl;
+    cout << "Você acertou o número secreto em" << attempts << " tentativas" << endl;
 
     return 0;
 }
