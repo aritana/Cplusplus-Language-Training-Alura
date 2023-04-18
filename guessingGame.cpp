ef5fed1458d620f88ee5d459cc558846025f516d
Aritana Noara Costa Santos <aritanacs@ciandt.com>
2023-04-17 23:30:46 -0300

    ending the guessing game

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <cmath>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 int main()
@@ -8,16 +9,37 @@ int main()
     cout << "* Welcome to the Guessing Game! *" << endl;
     cout << "**************************************" << endl;
 
-    const int SECRET_NUMBER = 42;
-    bool didnotHit = true;
+    cout << "Escolha o seu nível de dificuldade" << endl;
+    cout << "FÁCIL (F), Médio (M), difícil (D)" << endl;
+
+    char level;
+    cin >> level;
+
+    int numberOfAttempts;
+
+    if (level == 'F')
+    {
+        numberOfAttempts = 15;
+    }
+    else if (level == 'M')
+    {
+        numberOfAttempts = 10;
+    }
+    else
+    {
+        numberOfAttempts = 5;
+    }
+
+    srand(time(NULL));
+    const int SECRET_NUMBER = rand() % 100;
+
     int attempts = 0;
-
     double points = 1000.0;
+    bool hit = false;
 
-    do
+    for (attempts = 1; attempts <= numberOfAttempts; attempts++)
     {
         int guess;
-        attempts++;
 
         cout << "Tentativa " << attempts << endl;
         cout << "What is your guess? ";
@@ -27,13 +49,13 @@ int main()
         double lostPoints = abs(guess - SECRET_NUMBER) / 2.0;
         points = (points - lostPoints);
 
-        bool hit = (guess == SECRET_NUMBER);
+        hit = (guess == SECRET_NUMBER);
         bool greater = (guess > SECRET_NUMBER);
 
         if (hit)
         {
             cout << "Congratulations! You guessed corretly!" << endl;
-            didnotHit = false;
+            break;
         }
         else if (greater)
         {
@@ -43,13 +65,21 @@ int main()
         {
             cout << "You guess is less than the secret number!" << endl;
         }
-    } while (didnotHit);
+    }
 
     cout << "Fim de Jogo!" << endl;
-    cout << "Você acertou o número secreto em " << attempts << " tentativas" << endl;
-    cout.precision(2);
-    cout << fixed;
-    cout << "Sua pontuação foi de " << points << " pontos." << endl;
+
+    if (!hit)
+    {
+        cout << "Você perdeu, tente novamente!" << endl;
+    }
+    else
+    {
+        cout << "Você acertou o número secreto em " << attempts << " tentativas" << endl;
+        cout.precision(2);
+        cout << fixed;
+        cout << "Sua pontuação foi de " << points << " pontos." << endl;
+    }
 
     return 0;
 }
